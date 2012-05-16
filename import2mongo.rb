e17a08c6ac58959820ad6d89195a9affe88a11ad
Łukasz Wyszomirski <lwyszomirski@gmail.com>
2012-05-16 15:41:01 +0200

    dodanie wysylania paczek do couchdb

---
+++ import2mongo.rb
@@ -0,0 +1,20 @@
+require 'csv'
+require 'mongo'
+
+filename = ARGV[0]
+bazaDanych = ARGV[1]
+kolekcja = ARGV[2]
+portmongo = ARGV[3]
+
+csvAA = CSV.read(filename)
+headersA = csvAA.shift 
+csvAH = csvAA.map {|row| Hash[*headersA.zip(row).flatten] }
+
+@db = Mongo::Connection.new("localhost", portmongo.to_s).db(bazaDanych.to_s)
+@collection = @db.collection(kolekcja.to_s)
+@collection.remove
+csvAH.each do |pojedunczeUFO|
+	@collection.insert(pojedunczeUFO);
+end
+
+puts "zaimportowal dane"
---
+++ mongo2couch.rb
@@ -0,0 +1,36 @@
+require 'json'
+require 'mongo'
+require 'couchrest'
+
+iloscPakietow = ARGV[0].to_f
+bazaDanych = ARGV[1]
+portCouchDb = ARGV[2]
+
+kolekcja = ARGV[3]
+portMongo = ARGV[4]
+
+@dbmongo = Mongo::Connection.new("localhost", portMongo).db(bazaDanych)
+@collection = @dbmongo.collection(kolekcja)
+
+hash = []
+poczatek = 1
+licznik = @collection.count
+
+@couch = CouchRest.new("http://127.0.0.1:"+portCouchDb.to_s)
+@dbcouch = @couch.database!(bazaDanych)
+@dbcouch.recreate!
+while poczatek * iloscPakietow - iloscPakietow < licznik do
+	hash = @collection.find(nil, {:limit => iloscPakietow, :skip => (poczatek * iloscPakietow - iloscPakietow )}).to_a	
+	hash.collect do |idRemove|
+		idRemove.delete("_id")
+	end
+	@dbcouch.bulk_save(hash)
+	puts "skopiowano "+(poczatek*iloscPakietow).to_s
+	poczatek+=1	
+end
+
+#@collection.find.each do |jednoUFO|
+#		jednoUFO = jednoUFO.to_hash
+#		jednoUFO['_id'] = jednoUFO['_id'].to_s
+#		hash << jednoUFO 
+#end
